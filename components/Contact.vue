c448cca658320d1775e07e7a044aa143f8e6b0c2
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2022-09-13 11:52:44 +0530

    add contact section

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,13 +1,38 @@
 <template>
-  <section class="flex">
-    <div class="flex flex-col">
-      <p>Email</p>
+  <section class="bg-lavender flex items-center px-32 py-4 m-4 space-x-5">
+    <div class="social flex justify-end flex-1 space-x-4 text-4xl">
+      <!-- Add the style and icon you want using the String format -->
+      <a href="https://github.com/Chaitanya-Raj">
+        <font-awesome-icon icon="fa-brands fa-github fa-3x" />
+      </a>
+
+      <a href="https://www.linkedin.com/in/chaitanya-raj-4b3285187/">
+        <font-awesome-icon icon="fa-brands fa-linkedin fa-3x" />
+      </a>
+
+      <a href="https://www.instagram.com/_chaitanya.raj/">
+        <font-awesome-icon icon="fa-brands fa-instagram fa-3x" />
+      </a>
+
+      <a href="https://twitter.com/0xClef">
+        <font-awesome-icon icon="fa-brands fa-twitter fa-3x" />
+      </a>
+
+      <a href="mailto:chaitanyarajdixit@gmail.com">
+        <font-awesome-icon icon="fa-solid fa-envelope fa-3x" />
+      </a>
+
+      <a href="static/Chaitanya-Raj.pdf" target="_blank">
+        <font-awesome-icon icon="fa-solid fa-file-alt fa-3x" />
+      </a>
     </div>
     <!-- TODO: add form -->
-    <div class="flex flex-col">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur eos,
-      recusandae dolore iste, eveniet nam adipisci beatae eius illo numquam quod
-      a repellat enim excepturi laboriosam. Suscipit corporis ipsam tenetur.
+    <div class="flex flex-col flex-1 pl-5 border-l border-black">
+      If you need a modern and powerful website for your business, startup or
+      personally, I am available for work. You can email me directly at
+      <a href="mailto:chaitanyarajdixit@gmail.com"
+        >chaitanyarajdixit@gmail.com</a
+      >
     </div>
   </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,34 @@ import Landing from "~/components/Landing.vue";
 import Projects from "~/components/Projects.vue";
 import About from "~/components/About.vue";
 
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import {
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+/* import specific icons */
+import { faEnvelope, faFileAlt } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(
+  faTwitter,
+  faInstagram,
+  faLinkedin,
+  faGithub,
+  faEnvelope,
+  faFileAlt
+);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 export default Vue.extend({
   name: "IndexPage",
   components: { Landing, Projects, About },
