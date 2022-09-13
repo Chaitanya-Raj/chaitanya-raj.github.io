3db1b78a9a4e47b1ee9cf95b35b1abb34bcbbc80
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2022-09-13 11:32:12 +0530

    add about section

--- components/About.vue
+++ components/About.vue
@@ -1,15 +1,49 @@
 <template>
-  <section class="flex">
-    <div class="flex flex-col">
-      <h2>About Me</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quod
-        nostrum error facere veniam laudantium dolor ratione temporibus cum
-        impedit consequuntur, quasi ullam tenetur laboriosam nisi expedita
-        voluptatibus tempora dolorem.
-      </p>
+  <section class="min-h-min flex p-4 space-x-4">
+    <div class="flex flex-col space-y-5">
+      <h2 class="font-display text-5xl">Who Am I?</h2>
+      <div class="font-body flex flex-col space-y-4 text-xl">
+        <p>
+          Hi, I'm Chaitanya. I'm a web developer from New Delhi and a CS alumni
+          of the University of Delhi. I have experience with front-end
+          development in React/Vue, back-end development in Node.js, web design
+          in Figma and have also dabbled a bit in Python, bash scripting,
+          CyberSecurity, CTFs and Data Science.
+        </p>
+        <p>
+          I love playing
+          <a
+            href="https://lichess.org/@/ChaitanyaRaj"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Chess</a
+          >
+          and
+          <a
+            href="https://www.goodreads.com/user/show/97716082-chaitanya"
+            target="_blank"
+            rel="noopener noreferrer"
+            >reading any books</a
+          >
+          that I can lay my hands on. I'm currently learning UI Design and Figma
+          in my spare time.
+        </p>
+        <p>
+          I also love listening to
+          <a
+            href="https://open.spotify.com/artist/3pc0bOVB5whxmD50W79wwO"
+            target="_blank"
+            rel="noopener noreferrer"
+            >keshi</a
+          >
+          . He's my second favorite recording artist after lofi chillhop beats
+          to study/relax to ;)
+        </p>
+      </div>
     </div>
-    <img src="logo.svg" alt="" />
+    <div class="flex items-center">
+      <img src="headshot.webp" alt="headshot of chaitanya" class="w-full" />
+    </div>
   </section>
 </template>
 
