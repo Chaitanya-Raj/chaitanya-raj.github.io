fa5586fa39fea478a78063b0034503bd852e4b26
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2022-09-23 11:22:38 +0530

    preload images

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="md:flex flex-row items-center justify-between hidden pt-8">
     <a href="#home">
-      <img src="logo.svg" alt="Chaitanya Raj Logo" />
+      <nuxt-img src="logo.svg" alt="Chaitanya Raj Logo" placeholder />
     </a>
     <nav class="font-body md:pt-0 flex items-center pt-4 space-x-4 font-bold">
       <a href="#projects" class="highlight">my work</a>
